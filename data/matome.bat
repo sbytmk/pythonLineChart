a3f783acaa268df33f6b5411b421dfd0f1059c29
sbytmk <sbytmk@users.noreply.github.com>
2022-12-13 16:33:00 +0900

    Add files via upload

---
+++ data/matome.bat
@@ -0,0 +1,43 @@
+@echo off
+setlocal enabledelayedexpansion
+
+cd /d %~dp0
+
+del *.csv
+
+xcopy /S /D:12/9/22 X:\mold_info\mold_yokohama\nissei\MONDAT C:\Users\sbytm\Desktop\python_study\data
+
+rem ファイルはShift-JISが前提なので、文字コードがUTF-8の場合は設定
+rem chcp 65001
+
+ 
+rem ヘッダーを判定するための行カウント
+set /a cnt=0
+ 
+rem ファイル名を設定
+set FILE_NAME=matome.csv
+ 
+rem 移動したフォルダにあるCSVファイルを１つずつ処理
+for /f %%a in ('dir /b *.csv') do (
+    set TARGET_FILE=%%a
+    
+    rem ファイル名を出力
+    echo TARGET_FILE=!TARGET_FILE!
+ 
+    rem 1ファイル目であればヘッダーを出力
+    if !cnt!==0 (
+        rem 1行目を取得して出力
+        set /p header=<!TARGET_FILE!
+        echo !header!>>"!FILE_NAME!"
+    )
+    
+    rem カウントを追加
+    set /a cnt=!cnt!+1
+    
+    rem ファイルの1行目を飛ばして1行ずつ出力
+    for /f "usebackq delims= skip=1" %%b in ("!TARGET_FILE!") do (
+       echo %%b>>"!FILE_NAME!"
+    )
+)
+ 
+endlocal
