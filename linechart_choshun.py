deddaba554b75aaae1f1a0e02c8bd1163a493db6
sbytmk <sbytmk@users.noreply.github.com>
2022-12-20 07:09:42 +0900

    Update linechart_choshun.py

--- linechart_choshun.py
+++ linechart_choshun.py
@@ -24,8 +24,8 @@ select_df = today_df[['発生日', '射出最前進位置[mm]',
 select_df.columns = ['発生日時', '射出最前進位置[mm]', 'V-P切換圧[MPa]',
                      '射出最前進位置[mm]しきい値', 'V-P切換圧[MPa]しきい値']
 
-select_df['射出最前進位置[mm]しきい値'] = 1.89
-select_df['V-P切換圧[MPa]しきい値'] = 47.3
+select_df['射出最前進位置[mm]しきい値'] = 5.95
+select_df['V-P切換圧[MPa]しきい値'] = 75.0
 
 select_df1 = select_df[['発生日時', '射出最前進位置[mm]',
                         '射出最前進位置[mm]しきい値', '発生日時', 'V-P切換圧[MPa]', 'V-P切換圧[MPa]しきい値']]
@@ -47,7 +47,7 @@ labels = Reference(meisai, min_col=1, min_row=2, max_row=meisai.max_row)
 line.add_data(data, titles_from_data=True)
 line.set_categories(labels)
 
-line.x_axis.title = '射出最前進位置のしきい値は「1.89mm」　これ以上はショートリスク有り！'
+line.x_axis.title = '射出最前進位置のしきい値は「5.95mm」　これ以上はショートリスク有り！'
 
 
 ws.add_chart(line, 'A1')
@@ -65,7 +65,7 @@ labels2 = Reference(meisai, min_col=4, min_row=2, max_row=meisai.max_row)
 line2.add_data(data2, titles_from_data=True)
 line2.set_categories(labels2)
 
-line2.x_axis.title = 'VP切替圧のしきい値は「47.3Mpa」　これ以下はショートリスク有り！'
+line2.x_axis.title = 'VP切替圧のしきい値は「75.0Mpa」　これ以下はショートリスク有り！'
 
 
 ws.add_chart(line2, 'A40')
